757eae0b005763dfd8df4da70b262ad427961f48
cleiby <carl.leiby@gmail.com>
2011-02-02 13:50:12 -0500

    Started a new Rails3 project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Unscheduler::Application.config.secret_token = '3dfbc8991c63a4f362f4bc6bd49fad578bdfea3496ef681af620e7638776f7cbd8327bb01e1c345187272d26a303ca37d950240b4beb25735905703d1c146b3a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Unscheduler::Application.config.session_store :cookie_store, :key => '_unscheduler_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Unscheduler::Application.config.session_store :active_record_store
