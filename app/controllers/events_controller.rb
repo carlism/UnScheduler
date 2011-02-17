b1543d3c6194e3c85674f69be9a90806757d2d7e
cleiby <carl.leiby@gmail.com>
2011-02-17 13:43:38 -0500

    removed unused respond_to blocks as part of code review feedback

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,36 +2,22 @@ class EventsController < ApplicationController
   before_filter :authenticate_user!, :except => [:show, :index]
   def index
     @events = Event.find(:all)
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.xml  { render :xml => @events }
-    end
   end
   
   def new
     @event = Event.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.xml  { render :xml => @event }
-    end
   end
   
   def create
     @event = Event.new(params[:event])
-    @creator = Role.new(:role_type => Role::CREATOR_ROLE)
+    @creator = Role.new(:role_type => "creator")
     current_user.roles << @creator
 
-    respond_to do |format|
-      if @event.save
-        @event.roles << @creator
-        format.html { redirect_to(events_url, :notice => 'Event was successfully created.') }
-        format.xml  { render :xml => @event, :status => :created, :location => @event }
-      else
-        format.html { render :action => "new" }
-        format.xml  { render :xml => @event.errors, :status => :unprocessable_entity }
-      end
+    if @event.save
+      @event.roles << @creator
+      redirect_to(events_url, :notice => 'Event was successfully created.')
+    else
+      render :action => "new"
     end
   end
   
@@ -41,25 +27,16 @@ class EventsController < ApplicationController
 
   def update
     @event = Event.find(params[:id])
-
-    respond_to do |format|
-      if @event.update_attributes(params[:event])
-        format.html { redirect_to(event_path, :notice => 'Event was successfully updated.') }
-        format.xml  { head :ok }
-      else
-        format.html { render :action => "edit" }
-        format.xml  { render :xml => @event.errors, :status => :unprocessable_entity }
-      end
+    if @event.update_attributes(params[:event])
+      redirect_to(event_path, :notice => 'Event was successfully updated.')
+    else
+      render :action => "edit"
     end
   end
 
   def show
     @event = Event.find(params[:id], :include=>:event_dates)
-
-    respond_to do |format|
-      format.html { render :layout => "grid" }
-      format.xml  { render :xml => @event }
-    end
+    render :layout => "grid"
   end
 
   def destroy
@@ -74,9 +51,6 @@ class EventsController < ApplicationController
       flash[:alert] = "You need to be the creator of an event to delete it."              
     end
 
-    respond_to do |format|
-      format.html { redirect_to(events_url) }
-      format.xml  { head :ok }
-    end
+    redirect_to(events_url)
   end
 end
