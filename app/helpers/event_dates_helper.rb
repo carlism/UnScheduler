fae5689148faa7a5bb801070662d5aa49376a0db
cleiby <carl.leiby@gmail.com>
2011-02-21 07:54:37 -0500

    initial schedule grid port

--- app/helpers/event_dates_helper.rb
+++ app/helpers/event_dates_helper.rb
@@ -1,2 +1,21 @@
 module EventDatesHelper
+  def column_headings
+    if rotation == :time_on_top
+      "time_column_header"
+    else
+      "room_column_header"
+    end
+  end
+
+  def rotation_rows
+    if rotation == :time_on_top
+      "room_rows"
+    else
+      "time_rows"
+    end
+  end
+
+  def rotation
+    session[:rotation] ||= :time_on_top
+  end
 end
