68d3050a81371d3ee991dd790a87a8e4d028cac0
cleiby <carl.leiby@gmail.com>
2011-03-19 21:36:37 -0400

    Cleaned up room and time slot forms.

--- app/models/time_slot.rb
+++ app/models/time_slot.rb
@@ -1,4 +1,14 @@
 class TimeSlot < ActiveRecord::Base
   belongs_to :event_date
-  has_many :presentations
+  has_many :presentations, :dependent => :destroy
+  
+  DISPLAY_FORMAT = "%I:%M %p"
+  
+  def formatted_start_time
+    start_time.strftime(DISPLAY_FORMAT) if start_time
+  end
+  
+  def formatted_start_time=(input)
+     self.start_time = input
+  end
 end
