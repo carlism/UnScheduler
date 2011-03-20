e5a0e333a89034af9e1e7d8ac429ec62cce7b9d6
cleiby <carl.leiby@gmail.com>
2011-03-19 21:44:39 -0400

    fix ordering of rows and columns

--- app/models/event_date.rb
+++ app/models/event_date.rb
@@ -2,12 +2,13 @@ class EventDate < ActiveRecord::Base
   belongs_to :event
   validates_presence_of :event_date
 
-  has_many :time_slots
-  has_many :rooms
+  has_many :time_slots, :order => :start_time
+  has_many :rooms, :order => [:position, :name]
 
   def build_grid
     presentations = Presentation.joins(:time_slot).
-      where("time_slots.event_date_id = ?", self.id)
+      where("time_slots.event_date_id = ?", self.id).
+      includes( [:room, :time_slot] )
     presentations.inject({}) do |grid, presentation|
       grid[[presentation.time_slot.id, presentation.room.id]] = presentation
       grid
