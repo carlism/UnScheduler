8c7eea78b753e5fd15eb50d61b102d671414033a
cleiby <carl.leiby@gmail.com>
2011-04-16 23:51:47 -0400

    Just commented out the bits causeing problems for now.

--- config/compass.rb
+++ config/compass.rb
@@ -14,10 +14,10 @@ preferred_syntax = :sass
 require 'fileutils'
 FileUtils.mkdir_p(Rails.root.join("tmp", "stylesheets"))
 
-Compass::AppIntegration::Rails.initialize!
+# Compass::AppIntegration::Rails.initialize!
 
-Rails.configuration.middleware.delete('Sass::Plugin::Rack')
-Rails.configuration.middleware.insert_before('Rack::Sendfile', 'Sass::Plugin::Rack')
+# Rails.configuration.middleware.delete('Sass::Plugin::Rack')
+# Rails.configuration.middleware.insert_before('Rack::Sendfile', 'Sass::Plugin::Rack')
 
 Rails.configuration.middleware.insert_before('Rack::Sendfile', 'Rack::Static',
     :urls => ['/stylesheets'],
