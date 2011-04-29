650413b686554a68f7473e6598c789a3639b3584
cleiby <carl.leiby@gmail.com>
2011-04-29 14:56:48 -0400

    changed the secret_token to get rid of a devise issue

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Unscheduler::Application.config.secret_token = '3dfbc8991c63a4f362f4bc6bd49fad578bdfea3496ef681af620e7638776f7cbd8327bb01e1c345187272d26a303ca37d950240b4beb25735905703d1c146b3a'
+Unscheduler::Application.config.secret_token = '42fbc8991c63a4f362f4bc6bd49fad578bdfea3496ef681af620e7638776f7cbd8327bb01e1c345187272d26a303ca37d950240b4beb25735905703d1c146b3a'
