535811e369e88cb49a920fb4a8632333c2930e17
cleiby <carl.leiby@gmail.com>
2011-04-28 21:11:47 -0400

    Initial cut at an iphone style.

--- lib/domain_constraints.rb
+++ lib/domain_constraints.rb
@@ -1,5 +1,5 @@
 class DomainConstraints
-  SITE_NAMES = ['www.unscheduler.com', 'unscheduler.com', 'unscheduler.dev', 'localhost']
+  SITE_NAMES = ['www.unscheduler.com', 'unscheduler.com', 'unscheduler.dev', 'localhost', 'www.splatcamp.com']
   def matches?(request)
     not SITE_NAMES.include? request.host
   end
