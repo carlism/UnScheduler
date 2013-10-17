c8caa848c3159a8f2bf2ec3eaeed39d24352008a
Carl Leiby <carl.leiby@gmail.com>
2013-10-17 11:12:40 -0400

    Taking a whack at fixing domain routing.

--- lib/domain_constraints.rb
+++ lib/domain_constraints.rb
@@ -1,6 +1,5 @@
 class DomainConstraints
-  SITE_NAMES = ['www.unscheduler.com', 'unscheduler.com', 'unscheduler.dev', 'localhost', 'www.splatcamp.com']
   def matches?(request)
-    not SITE_NAMES.include? request.host
+    Event.find_by_host(request.host)
   end
 end
