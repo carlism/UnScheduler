4dbc6bb3cb9dd2c79b947db1d0507233c7b3b09c
Carl Leiby <carl.leiby@gmail.com>
2013-10-17 10:52:18 -0400

    Fixing the bcrypt change that made password salt irrelevant and adding unicorn.conf

---
+++ db/migrate/20131017141353_remove_salt_from_user.rb
@@ -0,0 +1,8 @@
+class RemoveSaltFromUser < ActiveRecord::Migration
+  def self.up
+  	remove_column :users, :password_salt
+  end
+
+  def self.down
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110425145213) do
+ActiveRecord::Schema.define(:version => 20131017141353) do
 
   create_table "domains", :force => true do |t|
     t.integer  "event_id"
@@ -78,7 +78,6 @@ ActiveRecord::Schema.define(:version => 20110425145213) do
   create_table "users", :force => true do |t|
     t.string   "email",                               :default => "", :null => false
     t.string   "encrypted_password",   :limit => 128, :default => "", :null => false
-    t.string   "password_salt",                       :default => "", :null => false
     t.string   "reset_password_token"
     t.string   "remember_token"
     t.datetime "remember_created_at"
