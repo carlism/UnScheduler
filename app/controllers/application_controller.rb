6d94fe3abeee1be280927065b45c81414e509d34
Carl Leiby <carl.leiby@cable.comcast.com>
2014-10-29 11:12:23 -0400

    disable iphone style

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,27 +2,27 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   layout :site_layout
 
-  before_filter :setup_for_iphone
-  helper_method :is_iphone?
+  #before_filter :setup_for_iphone
+  #helper_method :is_iphone?
   
-  def setup_for_iphone
-    if is_iphone? && !request.xhr?  
-      request.formats.unshift :html
-      request.formats.unshift :iphone
-      logger.debug "iphone formats: #{request.formats.inspect}"
-      session[:iphone_view] = true
-    end  
-  end
-  
-  def is_iphone?
-    request.user_agent.to_s =~ /iphone|ipod/i
-  end
-
-  def site_layout
-    if is_iphone?      
-      "iphone"
-    else
-      "application"
-    end
-  end
+  # def setup_for_iphone
+  #   if is_iphone? && !request.xhr?
+  #     request.formats.unshift :html
+  #     request.formats.unshift :iphone
+  #     logger.debug "iphone formats: #{request.formats.inspect}"
+  #     session[:iphone_view] = true
+  #   end
+  # end
+  #
+  # def is_iphone?
+  #   request.user_agent.to_s =~ /iphone|ipod/i
+  # end
+  #
+  # def site_layout
+  #   if is_iphone?
+  #     "iphone"
+  #   else
+  #     "application"
+  #   end
+  # end
 end
